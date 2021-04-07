a6b84d607d8e40d82570631c27bfe28f4acef569
jcoruepersonal <jcorue@hotmail.com.ar>
2021-04-07 10:16:41 -0300

    nuevo commit

---
+++ Funciones.js
@@ -0,0 +1,49 @@
+function Operacion(op, n1, n2)
+{
+    if (isNaN(parseFloat(n1))) {
+        alert('Ingresar un numero en el campo Numero 1');
+    }
+
+    else if (isNaN(parseFloat(n2))) {
+        alert('Ingresar un numero en en el campo Numero 2');
+    }
+
+    else {
+
+        switch (op) {
+
+            case 'suma':
+                var resultado = parseFloat(n1) + parseFloat(n2);
+                alert('La suma es ' + resultado);
+                break;
+
+            case 'resta':
+                var resultado = parseFloat(n1) - parseFloat(n2);
+                alert('La resta es ' + resultado);
+                break;
+
+            case 'multiplicacion':
+                var resultado = parseFloat(n1) * parseFloat(n2);
+                alert('La multiplicacion es ' + resultado);
+                break;
+
+            case 'division':                
+
+                if (n2 == 0) {
+
+                    alert('No se puede dividir por cero');
+                }
+
+                else {
+                    var resultado = parseFloat(n1) / parseFloat(n2);
+                    alert('La division es ' + resultado);
+                }                
+                break;
+				
+				
+				
+
+        }
+
+    }
+}
